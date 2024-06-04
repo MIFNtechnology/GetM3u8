7b474cf567f54521c6db80e71d5e8908fabaa565
Kaitou E <102132258+ejunds@users.noreply.github.com>
2024-06-05 00:50:56 +0800

    Update grabber.py
    Signed-off-by: Kaitou E <102132258+ejunds@users.noreply.github.com>

--- grabber.py
+++ grabber.py
@@ -165,10 +165,11 @@ def grab_twitch(url: str):
 channel_name = ''
 channel_id = ''
 category = ''
+channel_loga = ''
 
 # Open text file and parse stream information and URL
 with open('./streams.txt', encoding='utf-8') as f:
-    print("#EXTM3U")
+    print("#EXTM3U") ,print(tvg-id="https://raw.githubusercontent.com/MIFNtechnology/GetM3u8/main/epg.xml")
     for line in f:
         line = line.strip()
         if not line or line.startswith('##'):
