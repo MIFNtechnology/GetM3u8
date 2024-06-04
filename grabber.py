eeda9592cab7b04c42197841b1031c7692aa116f
Kaitou E <102132258+ejunds@users.noreply.github.com>
2024-06-04 23:42:25 +0800

    Update grabber.py
    Signed-off-by: Kaitou E <102132258+ejunds@users.noreply.github.com>

--- grabber.py
+++ grabber.py
@@ -8,7 +8,7 @@ import requests
 from lxml import etree
 from bs4 import BeautifulSoup
 
-tz = pytz.timezone('Europe/London')
+tz = pytz.timezone('Asia/Malaysia')
 channels = []
 
 
@@ -169,7 +169,7 @@ channel_logo = ''
 
 # Open text file and parse stream information and URL
 with open('./streams.txt', encoding='utf-8') as f:
-    print("#EXTM3U" tvg-url="https://raw.githubusercontent.com/MIFNtechnology/GetM3u8/main/epg.xml" )
+    print("#EXTM3U" ,tvg-url="https://raw.githubusercontent.com/MIFNtechnology/GetM3u8/main/epg.xml" )
     for line in f:
         line = line.strip()
         if not line or line.startswith('##'):
